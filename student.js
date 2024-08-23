262950df1a471ee7516bd171097626ae5c2a2e36
thegrungler <c.justliketheword@gmail.com>
2024-08-22 19:14:56 -0700

    add lab 2

--- student.js
+++ student.js
@@ -1,4 +1,12 @@
+var the_name = "Collin Necessary";
+var my_info = "cs190 Javascript Lab 2";
+var value2 = 488;
+
 function confirmlink251(linkname) {
     if (confirm("Are you sure you want to jump to " + linkname + "?"))
         location = linkname;
   } 
+
+  function displayinfo286() {
+    alert(the_name+"\n"+my_info+"\n"+value2);
+  }
