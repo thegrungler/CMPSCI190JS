5bc892b182602025b051c004e2d71744fa3d0d81
thegrungler <c.justliketheword@gmail.com>
2024-08-29 10:27:26 -0700

    Lab 4

--- student.js
+++ student.js
@@ -2,6 +2,7 @@ var the_name = "Collin Necessary";
 var my_info = "cs190 Javascript Lab 2";
 var value2 = 488;
 var currnum = 1;
+var custtype = "direct";
 
 function confirmlink251(linkname) {
     if (confirm("Are you sure you want to jump to " + linkname + "?"))
@@ -11,6 +12,33 @@ function confirmlink251(linkname) {
 function displayinfo286() {
     alert(the_name+"\n"+my_info+"\n"+value2);
   }
+
+function changetype350() {
+  var temp = "";
+  if (custtype == "direct") {
+    temp = "advertising";
+  }
+  if (custtype == "advertising") {
+    temp = "subscription";
+  }
+  if (custtype == "subscription") {
+    temp = "direct";
+  }
+  custtype = temp;
+  document.getElementById("customerType").innerHTML = "<h3>Customer type: "+custtype+"</h3>"
+}
+
+function displaytype721() {
+  if (custtype == "direct") {
+    alert("Buy Now! From this web page! My children need new shoes!");
+  }
+  if (custtype == "advertising") {
+    alert(" Support our advertisers! Click on an ad, so I can make money!");
+  }
+  if (custtype == "subscription") {
+    alert("Renew your subscription today! My children need medicine!");
+  }
+}
 
 function displayspecial447() {
 
@@ -27,5 +55,4 @@ function displayspecial447() {
     if (currnum > 3) {
     currnum = 1 ;  // wrap around
     }
- 
- }
+}
