61b616eac1ebe40c1d33c45f4e9c06d3c24736dc
thegrungler <c.justliketheword@gmail.com>
2024-09-03 09:59:25 -0700

    Add Page 2

---
+++ Page2/student.js
@@ -0,0 +1,13 @@
+function makeHeader() {
+    var data = "<table style='width:100%'><tr style='background-color:#0c0a26'><td >";
+    data +="<h1 style='text-align:center'>BetterThanAmazon - Product Description Page</h1>";
+    data +="</td></tr></table>";
+    return(data);
+}
+
+function makeFooter() {
+    var data = "<table style='width:100%' ><tr style='background-color:#0c0a26'><td>";
+    data +="<p style='text-align:center;font-size:10px'><b>Copyright 2024 BetterThanAmazon</b></p>";
+    data +="</td></tr></table>";
+    return(data);
+}  
