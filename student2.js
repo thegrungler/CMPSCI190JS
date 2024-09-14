ebd6c8c2c77adcad6ed0f1c3e69b3b17d89c2e91
thegrungler <c.justliketheword@gmail.com>
2024-09-14 12:45:04 -0700

    Lab 8

--- student2.js
+++ student2.js
@@ -3,10 +3,15 @@ address5 = "321 Main St. Valencia, CA 91344";
 phonenumber2 = "(555) 257-1234"
 
 function getHeader() {
-    var data = "<table style='width:100%'><tr style='background-color:#0c0a26'><td >";
+    var data ="<h3 style='text-align:right'>";
+    var now = new Date();
+    data+=" "+now.toLocaleString();
+    data+="</h3>"
+    data+= "<table style='width:100%'><tr style='background-color:#0c0a26'><td >";
     data +="<h1 style='text-align:center'>";
     data+=companyname3;
-    data +="</h1></td></tr></table>";
+    data +="</h1>"
+    data+="</td></tr></table>";
     return(data);
 }
 
@@ -14,6 +19,9 @@ function getFooter() {
     var data = "<table style='width:100%' ><tr style='background-color:#0c0a26'><td>";
     data +="<p style='text-align:center;font-size:10px'><b>Copyright 2024 ";
     data +=companyname3;
+    data +=", "+navigator.appName;
+    data +=", "+navigator.appVersion;
+    data +=", "+navigator.platform;
     data +="<br>";
     data +=address5;
     data +="<br>";
@@ -22,3 +30,21 @@ function getFooter() {
     data +="</td></tr></table>";
     return(data);
 } 
+
+function makeMenu4(size) {
+    var temp = "My Menu: "
+    for (var i = 0; i < size; i++) {
+        temp+="Button "
+    }
+    return(temp);
+}
+
+function makeMain7() {
+    var temp = "My product: <br>"
+    temp+="Product Name: <br>"
+    temp+="Product ID: <br>"
+    temp+="Product Description: <br>"
+    temp+="Shopping Cart Link Bar <br>"
+    temp+="Product Image: <br>"
+    return(temp);
+}
