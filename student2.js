dcc94af359ac717a12324f14567e06274b0a859d
thegrungler <c.justliketheword@gmail.com>
2024-09-18 17:05:56 -0700

    Lab 9

--- student2.js
+++ student2.js
@@ -2,6 +2,10 @@ companyname3 = "DVD Sales LTD";
 address5 = "321 Main St. Valencia, CA 91344";
 phonenumber2 = "(555) 257-1234"
 
+var product1 = { name:"HP 4426", id:"4426", desc:"Newest and Best Laptop from HP Computer"};
+var product2 = { name:"Apple 88123 iPad", id:"88123", desc:"Apple Tablet Computer"};
+var product3 = { name:"Dell Dimension 2400", id:"2400X", desc:"A fast 2.4 ghz computer"};
+
 function getHeader() {
     var data ="<h3 style='text-align:right'>";
     var now = new Date();
@@ -32,18 +36,27 @@ function getFooter() {
 } 
 
 function makeMenu4(size) {
-    var temp = "My Menu: "
+    var temp = "<span><strong>Menu: </strong>"
     for (var i = 0; i < size; i++) {
-        temp+="Button "
+        temp +="<button "
+        temp +="onClick=\"Javascript:execButton733(product"
+        temp +=(i+1)
+        temp +=")\""
+        temp +=">Product #"+(i+1)+"</button>"
     }
+    temp += "</span>"
     return(temp);
 }
 
-function makeMain7() {
-    var temp = "My product: <br>"
-    temp+="Product Name: <br>"
-    temp+="Product ID: <br>"
-    temp+="Product Description: <br>"
+function execButton733(product) {
+    document.getElementById("main925").innerHTML = makeMain7(product)
+}
+
+function makeMain7(myproduct) {
+    var temp = "<strong>My product:</strong> <br><br>"
+    temp+="Product Name: <strong>"+myproduct.name+"</strong><br>"
+    temp+="Product ID: <strong>"+myproduct.id+"</strong><br>"
+    temp+="Product Description: <strong>"+myproduct.desc+"</strong><br>"
     temp+="Shopping Cart Link Bar <br>"
     temp+="Product Image: <br>"
     return(temp);
