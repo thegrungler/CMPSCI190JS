69cc10e5f3a7362d6b7ae6547be037fec1160bd1
thegrungler <c.justliketheword@gmail.com>
2024-10-21 17:24:40 -0700

    Lab 1 php

---
+++ php/index.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html>
+<head><meta charset="UTF-8"></head>
+<body>
+<?php
+/*
+ * Who: Collin Necessary
+ * What: CMP SCI cs192 Lab Web Page Project
+ * When: MM/DD/YY  
+ * What: Lab Web Page #1
+*/
+// This is where you write the php for the lab
+    print "<h2>Lab 1 Assignment</h2>";
+    print "<p>My name is Collin Necessary<p>"
+?>
+</body>
+</html>
+<!-- Collin Necessary, CMP SCI cs192 Lab Web Page Project, Lab Web Page #1  -->
